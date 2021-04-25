688c8f2fcdfa82beaae972d7151227a2c1a4b8f7
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-04-26 00:17:49 +0300

    First edition of creating the cards

---
+++ src/Pokecard.js
@@ -0,0 +1,18 @@
+import React , {Component} from 'react';
+import './Pokecard.css'
+
+class Pokecard extends Component {
+    render() {
+        const {name , img , type , exp} = this.props;
+        return (
+            <div className="Pokecard">
+                <h1>{name}</h1>
+                <img src={img} className="pokecard-img"/>
+                <p>Type: {type}</p>
+                <p>Exp: {exp}</p>
+            </div>      
+        )
+    }
+}   
+
+export default Pokecard;
